3e22c27f628fed5956926805065f328941f6929c
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2025-04-06 19:18:41 -0300

    add header

---
+++ framework/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from '../logo.svg';
+
+const Header: React.FC<{}> = ({}) => {
+        return (
+            <header>
+            <div className="logo">
+                <img src={logo} className="logo-img" alt="logo" />
+                <div className="logo-text">EchoBox</div>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">Enviar Feedback</a></li>
+                    <li><a href="#services">Ver Feedbacks</a></li>
+                    <li><button className="login-button">Login</button></li>
+                </ul>
+            </nav>
+        </header>
+        
+    );
+}
+export default Header;
