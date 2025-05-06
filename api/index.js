26ad65645655c43c305ec7dc8d9a23538012ff8d
giovanisims <giovani@simoes.pro>
2025-05-05 22:30:11 -0300

    added test route

--- api/index.js
+++ api/index.js
@@ -6,16 +6,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/usuarios', async (req, res) => {
+app.get('/feedback', async (req, res) => {
   try {
-    const resultado = await pool.query('SELECT * FROM "user"');
+    const resultado = await pool.query('SELECT * FROM feedback');
     res.json(resultado.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Erro ao buscar usuários');
+    res.status(500).send('Erro ao buscar feedback');
+  }
+});
+
+app.post('/feedback', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM feedback');
+    res.json(resultado.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar feedback');
   }
 });
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
+
