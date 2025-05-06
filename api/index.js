4d9c7e95fad1d223af4a4c97790c36f1045db3af
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2025-05-05 21:01:21 -0300

    add cors and json

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/usuarios', async (req, res) => {
   try {
